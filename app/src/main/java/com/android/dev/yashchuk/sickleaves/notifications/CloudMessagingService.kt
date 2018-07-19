a8a10a876d65365aed0c69a44c13946da972bb1a
Vitali Yashchuk <vitali_yashchuk@senla.eu>
2018-07-19 23:30:11 +0300

    add FCM push notifications

--- app/src/main/java/com/android/dev/yashchuk/sickleaves/notifications/CloudMessagingService.kt
+++ app/src/main/java/com/android/dev/yashchuk/sickleaves/notifications/CloudMessagingService.kt
@@ -9,28 +9,35 @@ import android.media.RingtoneManager
 import android.os.Build
 import android.support.v4.app.NotificationCompat
 import com.android.dev.yashchuk.sickleaves.R
+import com.android.dev.yashchuk.sickleaves.login.LoginActivity
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
-
-private const val TAG = "CloudMessagingService"
 
 class CloudMessagingService : FirebaseMessagingService() {
 
     override fun onMessageReceived(remoteMessage: RemoteMessage?) {
-        super.onMessageReceived(remoteMessage)
+        remoteMessage?.let { message ->
+            message.notification?.let { notification ->
+                sendNotification(notification.body)
+            }
+        }
     }
 
     private fun sendNotification(messageBody: String?) {
-        val intent = Intent(this, MainActivity::class.java)
+        val intent = Intent(this, LoginActivity::class.java)
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
-        val pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
+        val pendingIntent = PendingIntent.getActivity(
+                this,
+                0,
+                intent,
                 PendingIntent.FLAG_ONE_SHOT)
 
         val channelId = getString(R.string.default_notification_channel_id)
         val defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
         val notificationBuilder = NotificationCompat.Builder(this, channelId)
-                .setSmallIcon(R.drawable.ic_stat_ic_notification)
-                .setContentTitle("FCM Message")
+                .setSmallIcon(R.drawable.ic_notification)
                 .setContentText(messageBody)
                 .setAutoCancel(true)
                 .setSound(defaultSoundUri)
@@ -38,18 +45,17 @@ class CloudMessagingService : FirebaseMessagingService() {
 
         val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
-        // Since android Oreo notification channel is needed.
+        createNotificationChannel(notificationManager, channelId)
+
+        notificationManager.notify(0, notificationBuilder.build())
+    }
+
+    private fun createNotificationChannel(notificationManager: NotificationManager, channelId: String) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             val channel = NotificationChannel(channelId,
-                    "Channel human readable title",
+                    getString(R.string.default_notification_channel_name),
                     NotificationManager.IMPORTANCE_DEFAULT)
             notificationManager.createNotificationChannel(channel)
         }
-
-        notificationManager.notify(0 /* ID of notification */, notificationBuilder.build())
-    }
-
-    private fun createNotificationChannel() {
-
     }
 }
