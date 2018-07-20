e716191b59428a15783421e28e9ebbbee14d6236
Vitali Yashchuk <vitali_yashchuk@senla.eu>
2018-07-20 17:26:04 +0300

    add some tests

---
+++ app/src/test/java/com/android/dev/yashchuk/sickleaves/SickLeavesPresenterTest.kt
@@ -0,0 +1,42 @@
+package com.android.dev.yashchuk.sickleaves
+
+import com.android.dev.yashchuk.sickleaves.sickleaves.SickLeavesContract
+import com.android.dev.yashchuk.sickleaves.sickleaves.SickLeavesPresenter
+import com.android.dev.yashchuk.sickleaves.utils.Injection
+import org.junit.Before
+import org.junit.Test
+import org.mockito.Mock
+import org.mockito.Mockito.verify
+import org.mockito.Mockito.never
+import org.mockito.MockitoAnnotations
+
+
+class SickLeavesPresenterTest {
+
+    @Mock private lateinit var sickLeavesView: SickLeavesContract.View
+
+    private lateinit var sickLeavesPresenter: SickLeavesPresenter
+
+    @Before
+    fun setup() {
+        MockitoAnnotations.initMocks(this)
+    }
+
+    @Test
+    fun showLoading_true_shouldHideEmptyView_shouldShowLoading() {
+        sickLeavesPresenter = Injection.provideSickLeavesPresenter(sickLeavesView)
+        sickLeavesPresenter.showLoading(true)
+
+        verify(sickLeavesView).hideEmptyView()
+        verify(sickLeavesView).showLoading(true)
+    }
+
+    @Test
+    fun showLoading_false_shouldOnlyHideLoading() {
+        sickLeavesPresenter = Injection.provideSickLeavesPresenter(sickLeavesView)
+        sickLeavesPresenter.showLoading(false)
+
+        verify(sickLeavesView, never()).hideEmptyView()
+        verify(sickLeavesView).showLoading(false)
+    }
+}
