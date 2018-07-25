1b05b79b3a45efcb8302a8d566651aebebb8e51f
Vitali Yashchuk <vitali_yashchuk@senla.eu>
2018-07-25 14:24:48 +0300

    add ui test

--- app/src/androidTest/java/com/android/dev/yashchuk/sickleaves/SickLeavesTest.kt
+++ app/src/androidTest/java/com/android/dev/yashchuk/sickleaves/SickLeavesTest.kt
@@ -1,26 +1,30 @@
 package com.android.dev.yashchuk.sickleaves
 
 
+import android.support.annotation.IdRes
 import android.support.test.InstrumentationRegistry
 import android.support.test.espresso.Espresso.onView
 import android.support.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu
+import android.support.test.espresso.UiController
+import android.support.test.espresso.ViewAction
 import android.support.test.espresso.ViewInteraction
 import android.support.test.espresso.action.ViewActions.*
 import android.support.test.espresso.assertion.ViewAssertions.matches
 import android.support.test.espresso.contrib.RecyclerViewActions
-import android.support.test.rule.ActivityTestRule
-import android.support.test.runner.AndroidJUnit4
-import com.android.dev.yashchuk.sickleaves.sickleaves.SickLeavesActivity
 import android.support.test.espresso.matcher.ViewMatchers
 import android.support.test.espresso.matcher.ViewMatchers.*
+import android.support.test.rule.ActivityTestRule
+import android.support.test.runner.AndroidJUnit4
 import android.support.v7.widget.RecyclerView
 import android.support.v7.widget.Toolbar
 import android.view.View
 import android.widget.TextView
+import com.android.dev.yashchuk.sickleaves.sickleaves.SickLeavesActivity
 import org.hamcrest.Description
 import org.hamcrest.Matcher
 import org.hamcrest.Matchers
 import org.hamcrest.Matchers.allOf
+import org.hamcrest.Matchers.not
 import org.hamcrest.TypeSafeMatcher
 import org.junit.Rule
 import org.junit.Test
@@ -44,10 +48,26 @@ class SickLeavesTest {
     }
 
     @Test
-    fun createSickLeave_shouldShowInList() {
+    fun createSickLeave_close_delete_shouldNotShowInList() {
         createSickLeave(TEST_TITLE, TEST_DESCRIPTION)
 
         onView(withItemText(TEST_TITLE)).check(matches(isDisplayed()))
+
+        closeSickLeave()
+
+        onView(isRoot()).perform(waitFor(3000))
+
+        val status = InstrumentationRegistry.getTargetContext().getString(R.string.sick_list_item_status_closed)
+
+        onView(withItemId(R.id.status)).check(matches(withText(status)))
+
+        onView(isRoot()).perform(waitFor(1000))
+
+        deleteSickLeave()
+
+        onView(isRoot()).perform(waitFor(3000))
+
+        onView(withItemText(TEST_TITLE)).check(matches(not(isDisplayed())))
     }
 
     @Test
@@ -78,6 +98,22 @@ class SickLeavesTest {
                 InstrumentationRegistry.getTargetContext().getString(R.string.sick_list_toolbar_title_closed)
 
         matchToolbarTitle(title)
+    }
+
+    private fun closeSickLeave() {
+        onView(withId(R.id.recycler))
+                .perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(
+                        0,
+                        clickChildViewWithId(R.id.close_btn)
+                ))
+    }
+
+    private fun deleteSickLeave() {
+        onView(withId(R.id.recycler))
+                .perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(
+                        0,
+                        clickChildViewWithId(R.id.delete_btn)
+                ))
     }
 
     private fun showAllSickLeaves() {
@@ -118,11 +154,58 @@ class SickLeavesTest {
         }
     }
 
+    private fun withItemId(@IdRes id: Int): Matcher<View> {
+        return object : TypeSafeMatcher<View>() {
+            override fun matchesSafely(item: View?): Boolean {
+                return Matchers.allOf(
+                        isDescendantOfA(isAssignableFrom(RecyclerView::class.java)),
+                        withId(id)
+                ).matches(item)
+            }
+
+            override fun describeTo(description: Description?) {
+                description?.appendText("is Descendant of a RecyclerView with id $id")
+            }
+        }
+    }
+
     private fun matchToolbarTitle(title: CharSequence): ViewInteraction {
         return onView(
                 allOf(
                         isAssignableFrom(TextView::class.java),
                         withParent(isAssignableFrom(Toolbar::class.java))))
                 .check(matches(withText(title.toString())))
+    }
+
+    private fun clickChildViewWithId(@IdRes id: Int): ViewAction {
+        return object : ViewAction {
+            override fun getConstraints(): Matcher<View> {
+                return isAssignableFrom(View::class.java)
+            }
+
+            override fun getDescription(): String {
+                return "Click on view with id: $id"
+            }
+
+            override fun perform(uiController: UiController?, view: View?) {
+                view?.findViewById<View>(id)?.performClick()
+            }
+        }
+    }
+
+    private fun waitFor(millis: Long): ViewAction {
+        return object : ViewAction {
+            override fun getConstraints(): Matcher<View> {
+                return isAssignableFrom(View::class.java)
+            }
+
+            override fun getDescription(): String {
+                return "Wait for $millis milliseconds"
+            }
+
+            override fun perform(uiController: UiController?, view: View?) {
+                uiController?.loopMainThreadForAtLeast(millis)
+            }
+        }
     }
 }
