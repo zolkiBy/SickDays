7d86804fcb0f7efc88ea2e70c80aa10d3fc2f292
Vitali Yashchuk <vitali_yashchuk@senla.eu>
2018-08-03 17:55:40 +0300

    add event observer

---
+++ app/src/main/java/com/android/dev/yashchuk/sickleaves/utils/EventObserver.kt
@@ -0,0 +1,11 @@
+package com.android.dev.yashchuk.sickleaves.utils
+
+import android.arch.lifecycle.Observer
+
+class EventObserver<T>(private val onEventUnhandledContent: (T) -> Unit) : Observer<Event<T>> {
+    override fun onChanged(event: Event<T>?) {
+        event?.getContentIfNotHandled()?.let { value ->
+            onEventUnhandledContent(value)
+        }
+    }
+}
